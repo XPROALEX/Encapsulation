b9c2adf08ef41ef3f61c5c4ad41a14a8a9f8a454
Alex <alex97av@gmail.com>
2023-07-27 21:43:14 +0200

    add obj to House.java and get for all variables

--- src/House.java
+++ src/House.java
@@ -1,2 +1,25 @@
 public class House {
+    private int floorsNumber;
+    private String address;
+    private String[] residentsNames;
+   /*public House(int floorsNumber,String address,String[] residentsNames){
+        this.floorsNumber=floorsNumber;
+        this.address=address;
+        this.residentsNames=residentsNames;
+    }*/
+   public House(){
+
+   }
+
+    public int getFloorsNumber() {
+        return floorsNumber;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public String[] getResidentsNames() {
+        return residentsNames;
+    }
 }
